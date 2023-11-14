5ac3a40f36ed602979c04903ec09c3930b85c0cf
Okamura-Koudai <koudaisuper@icloud.com>
2023-11-14 18:25:51 +0900

    ホーム画面基礎UI進めた

--- src/Home.js
+++ src/Home.js
@@ -1,29 +1,18 @@
-//メインコンテンツ
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Header from './Header';
-
-// 相談の一覧を表示するためのライブラリ
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
 import Avatar from '@mui/material/Avatar';
 import { red } from '@mui/material/colors';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Box from '@mui/material/Box';
-
-// 区切り線を表示するためのライブラリ
-import { Divider } from '@mui/material';
-// 画像のインポート
+import IconButton, { IconButtonProps } from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 import logo from "./Images/logo.png"
-// 投稿ボタンを表示するためのライブラリ
 import SpeedDial from '@mui/material/SpeedDial';
 import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import Fab from '@mui/material/Fab';
@@ -31,53 +20,71 @@ import Fab from '@mui/material/Fab';
 
 function Home() {
 
-    const [posts, setPosts] = useState([]);
-
     return (
         <div>
             {/* ヘッダー */}
             <Header />
-            
+
             {/* コンテンツ */}
             <br></br>
             <Box
-                sx={{display: 'flex',justifyContent: 'center',alignItems: 'center',}}
+                sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', }}
             >
-                <Card sx={{ maxWidth: 500, maxHeight:300}}>
+                
+                <Card sx={{ maxWidth: 500 }}>
                     <CardHeader
                         avatar={
-                            <Avatar sx={{ bgcolor: '#BEDFC2' }} aria-label="guardian">
-                            </Avatar>
+                            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe"></Avatar>
+                        }
+                        action={
+                            <IconButton aria-label="settings"></IconButton>
                         }
                         title="タイトル"
+                        subheader="2023/11/14"
                     />
-                    <CardMedia component="img" height="100%" width="100%" image={logo} alt="image"/>
+                    <CardMedia
+                        component="img"
+                        height="194"
+                        image={logo}
+                        alt="image"
+                    />
+                    <CardContent>
+                        <Typography variant="body2" color="text.secondary">
+                            This impressive paella is a perfect party dish and a fun meal to cook
+                            together with your guests.
+                        </Typography>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="add to favorites">
+                            <FavoriteIcon />
+                        </IconButton>
+                    </CardActions>
                 </Card>
+
             </Box>
 
             {/* 右下固定投稿ボタン */}
             <Link to={"/create_post"}>
-                    <SpeedDial
-                        ariaLabel="SpeedDial basic example"
-                        sx={{
-                            position: 'fixed',
-                            bottom: 73,
-                            right: 18,
-                            zIndex: 9999,
-                        }}
-                        icon={
-                            <Fab
-                                sx={{
-                                    backgroundColor: '#BEDFC2', // 背景色を変更
-                                    color: 'black', // アイコンの色を変更
-                                }}
-                            >
-                                <SpeedDialIcon />
-                            </Fab>
-                        }
-                    >
-                    </SpeedDial>
-                </Link>
+                <SpeedDial
+                    ariaLabel="SpeedDial basic example"
+                    sx={{
+                        position: 'fixed',
+                        bottom: 73,
+                        right: 18,
+                        zIndex: 9999,
+                    }}
+                    icon={
+                        <Fab
+                            sx={{
+                                backgroundColor: '#BEDFC2', color: 'black',
+                            }}
+                        >
+                            <SpeedDialIcon />
+                        </Fab>
+                    }
+                >
+                </SpeedDial>
+            </Link>
         </div>
     );
 }
